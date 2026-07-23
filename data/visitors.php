06b385baa04c494f659cea1726a8c666dad62f23
755596985 <107170955+755596985@users.noreply.github.com>
2026-07-23 23:13:45 +0800

    添加/更新 data/visitors.php

---
+++ data/visitors.php
@@ -0,0 +1,331 @@
+<?php exit;?>
+[
+    {
+        "id": "6a6205577ccad",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:13:11"
+    },
+    {
+        "id": "6a620557e7570",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/index.php",
+        "time": "2026-07-23 20:13:11"
+    },
+    {
+        "id": "6a620558ea322",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/",
+        "time": "2026-07-23 20:13:12"
+    },
+    {
+        "id": "6a620563b04db",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/",
+        "time": "2026-07-23 20:13:23"
+    },
+    {
+        "id": "6a6205640e959",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/",
+        "time": "2026-07-23 20:13:24"
+    },
+    {
+        "id": "6a620565641e8",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=visitors",
+        "time": "2026-07-23 20:13:25"
+    },
+    {
+        "id": "6a62056e1b34d",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=visitors",
+        "time": "2026-07-23 20:13:34"
+    },
+    {
+        "id": "6a62056e3d793",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:13:34"
+    },
+    {
+        "id": "6a6205719864a",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/admin\/index.php",
+        "time": "2026-07-23 20:13:37"
+    },
+    {
+        "id": "6a6205724b95d",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php",
+        "time": "2026-07-23 20:13:38"
+    },
+    {
+        "id": "6a62057721dfe",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=visitors",
+        "time": "2026-07-23 20:13:43"
+    },
+    {
+        "id": "6a620577eb14c",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=posts",
+        "time": "2026-07-23 20:13:43"
+    },
+    {
+        "id": "6a620578bb094",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=comments",
+        "time": "2026-07-23 20:13:44"
+    },
+    {
+        "id": "6a620579b2665",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=config",
+        "time": "2026-07-23 20:13:45"
+    },
+    {
+        "id": "6a62057ab2219",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=filter",
+        "time": "2026-07-23 20:13:46"
+    },
+    {
+        "id": "6a62057bb556d",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=users",
+        "time": "2026-07-23 20:13:47"
+    },
+    {
+        "id": "6a62057cc7d20",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:13:48"
+    },
+    {
+        "id": "6a62057dcddd1",
+        "ip": "49.234.206.157",
+        "location": "上海 上海 腾讯云",
+        "ua": "curl\/8.4.0",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:13:49"
+    },
+    {
+        "id": "6a62059ab6400",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=visitors",
+        "time": "2026-07-23 20:14:18"
+    },
+    {
+        "id": "6a6205a666073",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/admin\/manage.php?tab=posts",
+        "time": "2026-07-23 20:14:30"
+    },
+    {
+        "id": "6a6205ab7c329",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:14:35"
+    },
+    {
+        "id": "6a620689f1c7e",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 20:18:17"
+    },
+    {
+        "id": "6a62068d9b153",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 20:18:21"
+    },
+    {
+        "id": "6a62068ed33a0",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 20:18:22"
+    },
+    {
+        "id": "6a62069201dd9",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 20:18:26"
+    },
+    {
+        "id": "6a6206c4c13c2",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:19:16"
+    },
+    {
+        "id": "6a6206edaed8d",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:19:57"
+    },
+    {
+        "id": "6a6207199de25",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:20:41"
+    },
+    {
+        "id": "6a620762d1185",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/?p=posts",
+        "time": "2026-07-23 20:21:54"
+    },
+    {
+        "id": "6a6209b8a4abd",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 20:31:52"
+    },
+    {
+        "id": "6a6209c1c80dd",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 20:32:01"
+    },
+    {
+        "id": "6a6209c693942",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 20:32:06"
+    },
+    {
+        "id": "6a620c127247f",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:41:54"
+    },
+    {
+        "id": "6a620cbd1de20",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/?p=home",
+        "time": "2026-07-23 20:44:45"
+    },
+    {
+        "id": "6a620d9901dc1",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; U; Android 13; zh-cn; PHJ110 Build\/TP1A.220905.001) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/115.0.5790.168 Mobile Safari\/537.36 HeyTapBrowser\/40.10.15.1",
+        "url": "http:\/\/ghb.xulove.top\/",
+        "time": "2026-07-23 20:48:25"
+    },
+    {
+        "id": "6a6224a6c71dc",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 22:26:46"
+    },
+    {
+        "id": "6a6224ac5093f",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 22:26:52"
+    },
+    {
+        "id": "6a6224c36c489",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 22:27:15"
+    },
+    {
+        "id": "6a6224dac1c0d",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 22:27:38"
+    },
+    {
+        "id": "6a6224ddbe3b2",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=posts",
+        "time": "2026-07-23 22:27:41"
+    },
+    {
+        "id": "6a6224e7d825c",
+        "ip": "223.104.79.130",
+        "location": "广东 深圳 中国移动\/基站WiFi",
+        "ua": "Mozilla\/5.0 (Linux; Android 12; HarmonyOS; BRQ-AN00; HMSCore 6.15.4.342) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/114.0.5735.196 HuaweiBrowser\/15.0.10.302 Mobile Safari\/537.36",
+        "url": "http:\/\/ghb.xulove.top\/index.php?p=home",
+        "time": "2026-07-23 22:27:51"
+    }
+]
